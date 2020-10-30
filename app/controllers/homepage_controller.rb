565b4326808af85a5ed75bf83c7b757ae80c282c
Vinith Reddy <67469723+vinithRreddy@users.noreply.github.com>
2020-10-30 12:23:37 +0530

    comments update

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,5 +1,4 @@
 class HomepageController < ApplicationController
-  skip_before_action :authorize
 
   def index
     @posts= Post.order(:title)
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ApplicationRecord
     validates :title, :body, presence: true
     validates :body, length: {minimum: 35}
-    
+    belongs_to :user
+    has_many :comments
     
 
 end
